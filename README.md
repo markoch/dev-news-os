8dd1208f1d5c2be994d7daa152156c398edf85a2
Marco Koch <marco.koch.hh@gmail.com>
2016-05-06 20:57:12 +0200

    Updates for open shift

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # Developer News
-[![Build Status](https://travis-ci.org/markoch/dev-news.svg?branch=master)](https://travis-ci.org/markoch/dev-news)
-[![Dependency Status](https://img.shields.io/david/markoch/dev-news.svg?style=flat)](https://david-dm.org/markoch/dev-news)
-[![devDependency Status](https://img.shields.io/david/dev/markoch/dev-news.svg?style=flat)](https://david-dm.org/markoch/dev-newsinfo=devDependencies)
+[![Build Status](https://travis-ci.org/markoch/dev-news-os.svg?branch=master)](https://travis-ci.org/markoch/dev-news-os)
+[![Dependency Status](https://img.shields.io/david/markoch/dev-news-os.svg?style=flat)](https://david-dm.org/markoch/dev-news-os)
+[![devDependency Status](https://img.shields.io/david/dev/markoch/dev-news-os.svg?style=flat)](https://david-dm.org/markoch/dev-news-os#info=devDependencies)
 
 Developer news website
 
--- app.js
+++ app.js
@@ -1,9 +1,5 @@
 /*eslint-env node*/
 
-//------------------------------------------------------------------------------
-// node.js starter application for Bluemix
-//------------------------------------------------------------------------------
-
 // This application uses express as its web server
 // for more info, see: http://expressjs.com
 var express = require('express');
@@ -11,8 +7,9 @@ var express = require('express');
 // create a new express server
 var app = express();
 
-var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
-var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
+// define openshift runtime environment
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 
 // serve the files out of ./public as our main files
 app.use(express.static(__dirname + '/public'));
@@ -20,5 +17,5 @@ app.use(express.static(__dirname + '/public'));
 // start server on the specified port and binding host
 app.listen(server_port, server_ip_address, function () {
   // print a message when the server starts listening
-  console.log( "Listening on " + server_ip_address + ", server_port " + server_port );
+  console.log( 'Listening on ' + server_ip_address + ', server_port ' + server_port );
 });
