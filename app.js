c2fc9ccb08ca716f840bb2645a9c5d8dc449a847
Marco Koch <marco.koch.hh@gmail.com>
2016-05-15 12:31:29 +0200

    Review existing implementation and adding news link
    Review existing implementation
     adding grunt, change to app location, changes to app.js, restructure
    package.json file
    Adding new page sample page

--- app.js
+++ app.js
@@ -1,18 +1,20 @@
 /*eslint-env node*/
 
-// This application uses express as its web server
-// for more info, see: http://expressjs.com
-var express = require('express');
+var express     = require('express');
+var compression = require('compression');
 
 // create a new express server
 var app = express();
 
 // define openshift runtime environment
-var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
+var server_port       = process.env.OPENSHIFT_NODEJS_PORT || 8080;
 var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 
+//use compression
+app.use(compression());
+
 // serve the files out of ./public as our main files
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/dist'));
 
 // start server on the specified port and binding host
 app.listen(server_port, server_ip_address, function () {
