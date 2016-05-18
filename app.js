05fe18e92c4020cc5a7ba9d4b35c52f44515aea1
Marco Koch <marco.koch.hh@gmail.com>
2016-05-18 08:15:41 +0200

    Revert changes

--- app.js
+++ app.js
@@ -1,15 +1,17 @@
 /*eslint-env node*/
 
 var express     = require('express');
+var compression = require('compression');
+
+// create a new express server
+var app = express();
 
 // define openshift runtime environment
 var server_port       = process.env.OPENSHIFT_NODEJS_PORT || 8080;
 var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 
-// create a new express server
-var app = express();
-//require('./server/config/express')(app);
-//require('./server/routes')(app);
+//use compression
+app.use(compression());
 
 // serve the files out of ./public as our main files
 app.use(express.static(__dirname + '/dist'));
