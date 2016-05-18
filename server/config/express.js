fb2801b7b4e853d8af3cd5dde91bc5f4515bd963
Marco Koch <marco.koch.hh@gmail.com>
2016-05-18 08:21:58 +0200

    Refactoring server part

--- server/config/express.js
+++ server/config/express.js
@@ -1,7 +1,14 @@
 'use strict';
 
+var morgan      = require('morgan');
 var compression = require('compression');
 
 module.exports = function(app) {
+    var env = app.get('env');
+
     app.use(compression());
+
+    if ('development' === env || 'test' === env) {
+        app.use(morgan('dev'));
+    }
 };
