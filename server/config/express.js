d8cdeadf6c4528109eb6571ad4c09dc510cb2865
Marco Koch <marco.koch.hh@gmail.com>
2016-05-18 18:31:56 +0200

    Adding more node packages
    Adding body-parser and cookie-parser

--- server/config/express.js
+++ server/config/express.js
@@ -3,11 +3,16 @@
 var express     = require('express');
 var morgan      = require('morgan');
 var compression = require('compression');
+var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
 
 module.exports = function(app) {
     var env = app.get('env');
 
     app.use(compression());
+    app.use(bodyParser.urlencoded({ extended: false }));
+    app.use(bodyParser.json());
+    app.use(cookieParser());
 
     // serve the files out of ./dist as our main files
     app.use(express.static(__dirname + '/../../dist'));
