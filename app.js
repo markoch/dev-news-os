a9444d6104eb1e9ec62e2c340dee2cd10c48dfec
Marco Koch <marco.koch.hh@gmail.com>
2016-05-20 23:35:39 +0200

    log node_env mode

--- app.js
+++ app.js
@@ -13,5 +13,5 @@ require('./server/routes')(app);
 
 // Start server on the specified port and binding host
 app.listen(server_port, server_ip_address, function () {
-  console.log( 'Listening on ' + server_ip_address + ', server_port ' + server_port );
+  console.log( 'Listening on ' + server_ip_address + ', server_port ' + server_port + ' in mode ' + process.env.NODE_ENV);
 });
