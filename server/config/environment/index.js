4f5adf18006670832cc118940a90b243e1336b84
Marco Koch <marco.koch.hh@gmail.com>
2016-05-24 19:59:06 +0200

    Change mongoldb configuration

--- server/config/environment/index.js
+++ server/config/environment/index.js
@@ -10,11 +10,27 @@ var all = {
     mongodb: {
         uri :   process.env.MONGOLAB_URI ||
                 process.env.OPENSHIFT_MONGODB_DB_URL+process.env.OPENSHIFT_APP_NAME ||
-                'mongodb://localhost/jsnews-dev'
+                'mongodb://localhost/devnews-dev'
     },
 
     // Should we populate the DB with sample data?
-    seedDB: false,
+    seedDB: true,
 };
+
+// default to a 'localhost' configuration:
+all.mongodb = {
+    uri: 'mongodb://localhost/devnews-dev'
+};
+
+// if OPENSHIFT env variables are present, use the available connection info:
+if (process.env.MONGOLAB_URI) {
+    all.mongodb.uri = process.env.MONGOLAB_URI;
+} else if(process.env.OPENSHIFT_MONGODB_DB_PASSWORD) {
+    all.mongodb.uri = 'mongodb://' + process.env.OPENSHIFT_MONGODB_DB_USERNAME + ':' +
+    process.env.OPENSHIFT_MONGODB_DB_PASSWORD + '@' +
+    process.env.OPENSHIFT_MONGODB_DB_HOST + ':' +
+    process.env.OPENSHIFT_MONGODB_DB_PORT + '/' +
+    process.env.OPENSHIFT_APP_NAME;
+}
 
 module.exports = all;
