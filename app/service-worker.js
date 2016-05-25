42a718f3047b2c01e45c916f3a538b86da78eb89
Marco Koch <marco.koch.hh@gmail.com>
2016-05-25 06:36:19 +0200

    Remove console statements

--- app/service-worker.js
+++ app/service-worker.js
@@ -4,7 +4,6 @@ var filesToCache = [
 ];
 
 self.addEventListener('install', function(e) {
-    console.log('[ServiceWorker] install');
   e.waitUntil(
     caches.open(cacheName)
     .then(function(cache) {
@@ -14,11 +13,9 @@ self.addEventListener('install', function(e) {
 });
 
 self.addEventListener('activate', function(e) {
-  console.log('[ServiceWorker] Activate');
   e.waitUntil(
     caches.keys().then(function(keyList) {
       return Promise.all(keyList.map(function(key) {
-        console.log('[ServiceWorker] Removing old cache', key);
         if (key !== cacheName) {
           return caches.delete(key);
         }
