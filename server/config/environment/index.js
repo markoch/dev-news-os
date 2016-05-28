fc99418e6ec4ff968e101e6d5c15c8d68136bb2f
Marco Koch <marco.koch.hh@gmail.com>
2016-05-28 07:42:19 +0200

    Remove duplicate mongoldb env setting

--- server/config/environment/index.js
+++ server/config/environment/index.js
@@ -6,13 +6,6 @@ var all = {
         host : process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
     },
 
-    // define mongodb runtime environment
-    mongodb: {
-        uri :   process.env.MONGOLAB_URI ||
-                process.env.OPENSHIFT_MONGODB_DB_URL+process.env.OPENSHIFT_APP_NAME ||
-                'mongodb://localhost/devnews-dev'
-    },
-
     // Should we populate the DB with sample data?
     seedDB: false
 };
